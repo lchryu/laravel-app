efa336f919cf31ffe5e9258f57cf56866674373f
Lucute2001 <lch.ryu2001@gmail.com>
2023-06-04 19:38:58 +0700

    45:37 - Configure Navigation and nav-item

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
     <p>This is Footer</p>
+    {{ print_r(URL('images/zenitsu.jpg'))}}
+    <img
+        src="{{ URL('images/zenitsu.jpg') }}"
+        width="400px"
+        height="250px"
+        alt="">
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,16 +2,24 @@
     <div class="container-fluid">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a href="/" class="nav-link">Home</a>
+                <a
+                    href="/"
+                    class="nav-link {{ request()->is('/') ? 'active' : '' }}">Home</a>
             </li>
             <li class="nav-item">
-                <a href="about" class="nav-link">About</a>
+                <a
+                    href="about"
+                    class="nav-link {{ request()->is('about') ? 'active'  : ''}}">About</a>
             </li>
             <li class="nav-item">
-                <a href="portfolio" class="nav-link">Portfolio</a>
+                <a
+                    href="portfolio"
+                    class="nav-link">Portfolio</a>
             </li>
             <li class="nav-item">
-                <a href="contact" class="nav-link">Contact</a>
+                <a
+                    href="contact"
+                    class="nav-link">Contact</a>
             </li>
         </ul>
     </div>
