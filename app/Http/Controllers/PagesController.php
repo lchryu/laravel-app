410d1cc249a9d20f9109d493eae64faff7c47802
Lucute2001 <lch.ryu2001@gmail.com>
2023-06-06 20:15:49 +0700

    50:57 - Some basic commands-syntax in Laravel Blade

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,10 +6,18 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('index');
     }
-    public function about() {
-        return view('about');
+
+    public function about()
+    {
+        $name = "LCH";
+        $names = array('LCH', 'Tony', 'David', 'Peter', 'John');
+        return view('about', [
+            'name' => $name,
+            'names' => $names,
+        ]);
     }
 }
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>This is About Page, with shared Header, Footer</h1>
+    <h1>This is About Page, with shared Header, Footer</h1>
+    {{
+        $x = 2
+    }}
+    @if($x > 2)
+        <h3>x is greater than 2</h3>
+    @elseif($x < 2)
+        <h3>x is less than 2</h3>
+    @else
+        <h3>x is equal to 2</h3>
+    @endif
+
+    {{--@unless(empty($name))--}}
+    {{--    <h3>name is not empty</h3>--}}
+    {{--    {{ $name }}--}}
+    {{--@endunless--}}
+    {{--    @if (!empty($name))--}}
+    {{--        <h3>name is not empty</h3>--}}
+    {{--        {{ $name }}--}}
+    {{--    @endif--}}
+    @empty(!$name)
+        <h3>name is not empty</h3>
+    @endempty
+    @empty($age)
+        <h3>age is empty</h3>
+    @endempty
+
+    @isset($name)
+        <h3>name is set</h3>
+    @endisset
+
+    @switch($name)
+        @case('John')
+            <h3>name is John</h3>
+            @break
+        @case('LCH')
+            <h3>Name is mr LCH</h3>
+            @break
+        @default
+            <h3>name is not John or Doe</h3>
+            @break
+
+    @endswitch
+    @for($i = 0; $i < 5; $i++)
+        <h2>i = {{ $i }}</h2>
+    @endfor
+    <h3>---------------------------</h3>
+    @foreach($names as $eachName)
+        <h3>{{ $eachName }}</h3>
+    @endforeach
 @endsection
